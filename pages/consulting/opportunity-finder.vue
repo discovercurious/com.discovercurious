99dc49adc5fa21e0416c72d4c3b872fe86a73f47
Bryce Howitson <bryce@discovercurious.com>
2019-07-31 17:01:22 -0500

    add sharing content to cop

--- pages/consulting/opportunity-finder.vue
+++ pages/consulting/opportunity-finder.vue
@@ -100,13 +100,13 @@ export default {
       title: 'Curious: Opportunity Mapping & Solution Identification',
       meta: [
 
-        { hid: 'og:title', name: 'og:title', content: 'Curious: Workshop - Digital Thinking for Analog Organizations' },
-        { hid: 'og:description', name: 'og:description', content: 'A foundational workshop providing teams with techniques to imagine and develop digital products that create unique innovative solutions in their organization or market.' },
+        { hid: 'og:title', name: 'og:title', content: 'Curious: Opportunity Mapping & Solution Identification' },
+        { hid: 'og:description', name: 'og:description', content: 'We created the Curious Opportunity Map (COP) process to identify ways to productize your unique business, service, and knowledge.' },
 
          { hid: 'og:image', name: 'og:image', content: 'https://discovercurious.com/workshops/cop.png' },
          { hid: 'og:image:type', name: 'og:image:type', content: 'image/png' },
          { hid: 'og:image:width', name: 'og:image:width', content: '1168' },
-         { hid: 'og:image:height', name: 'og:image:height', content: '1034' },
+         { hid: 'og:image:height', name: 'og:image:height', content: '722' },
       ]
     }
   },
