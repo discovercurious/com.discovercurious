ac626138b0bce518f2f8f2ff89da17e53c4819ff
Bryce Howitson <bryce@discovercurious.com>
2019-09-05 18:09:19 -0500

    add links

--- pages/insights/how-an-iterative-strategy-can-help-you-make-your-marketing-run-like-clockwork.vue
+++ pages/insights/how-an-iterative-strategy-can-help-you-make-your-marketing-run-like-clockwork.vue
@@ -6,9 +6,9 @@
           <div class="byline">
             <span class="author">Jason Rysavy</span> - <span class="date">09-05-2019</span>
           </div>
-          <div class="col2 s-textRow">
-              <p>In 2008, a small startup began its mission to sell grammar-checking software to universities. Unlike its technology counterparts, it wanted to avoid the need for venture capital and bootstrap its way to profitability. The startup’s plan? Test a highly targeted digital marketing strategy and improve on it from there. </p>
-              <p>Anyone who's been in the SaaS industry understands the need to get paying subscribers fast. For this startup, known as Grammarly, being self-funded multiplied the need for subscribers exponentially. To stay viable it needed cash flow quickly. This strategy required grit, ingenuity, and flexibility. </p>
+
+              <p>In 2008, a small startup began its mission to sell grammar-checking software to universities. Unlike its technology counterparts, it wanted to <a href="https://producthabits.com/how-grammarly-quietly-grew-its-way-to-7-million-daily-users/">avoid the need for venture capital</a> and bootstrap its way to profitability. The startup’s plan? Test a highly targeted digital marketing strategy and improve on it from there. </p>
+              <p>Anyone who's been in the SaaS industry understands the need to get paying subscribers fast. For this startup, known as <a href="https://www.grammarly.com/about">Grammarly</a>, being self-funded multiplied the need for subscribers exponentially. To stay viable it needed cash flow quickly. This strategy required grit, ingenuity, and flexibility. </p>
               <p>But, that’s only half the story. Grammarly needed to differentiate itself from its competitors. After all, products like Microsoft Word were on the market. How did they grow their business to $10 million, 3 years after it was founded?</p>
               <p>Cutting out the need for investments had its advantages. Instead of worrying about what investors thought, Grammarly was free to be user-focused and to do something with the feedback it was consistently getting. This helped Grammarly understand its customers and target them with a strategy that they would engage with.</p>
               <p>Another contributor to Grammarly’s success was its iterative strategy. Instead of going after new markets, they went after the low-hanging fruit. When growth started to flatten, Grammarly didn’t try to build a new product. Instead, they widened their reach from solely universities to a general audience. They realized their low-hanging fruit was a shift in positioning. After all, nobody wants to send an email or report that’s full of typos.</p>
@@ -17,7 +17,7 @@
               <h4>What is an iterative strategy?</h4>
               <blockquote>
                 <span class="quote">The essence of strategy is choosing what not to do.</span>
-                <span class="attr">Michael Porter</span>
+                <span class="attr"><a href="https://en.wikipedia.org/wiki/Michael_Porter">Michael Porter</a></span>
               </blockquote>
               <p>As Porter highlights, choosing which paths to avoid is just as important as which to pursue when picking your strategy. This makes testing potential solutions invaluable. If you don’t first identify low-performing options, you could get stuck with a strategy that’s a drain on resources instead of a boost to business performance.</p>
               <p>Choosing how you identify low-performing strategies is just as important of a decision. Traditionally, clients hire a business consultant to test and research every possibility, this consultant then creates a robust strategy. This approach is expensive and time-consuming. It can take several months and cost tens of thousands of dollars with additional costs for execution. This cost is more than the average small to medium sized business can handle.</p>
@@ -25,8 +25,8 @@
               <p>Another difference between iterative and traditional approaches is the opportunity for testing and change. With the traditional approach, the business strategy will only go through minimal adjustments once it’s set. The momentum from a failing strategy is nearly impossible to stop, which can lead to significant financial waste. On the other hand, iterative approaches give your business the opportunity to test multiple strategies and build on results instead of theories.</p>
 
               <h4>Your strategy starts with asking the right questions</h4>
-              <p>Imagine that a company produces vital signs monitors for healthcare organizations and they read an article on local SEO. They’re currently experiencing revenue problems and considering local SEO as the solution. While there may be enough demand locally, will there be enough local search volume to justify the effort and expense? It’s hard to know at first glance.</p>
-              <p>The problem is that we’re inundated with articles telling us that our business needs SEO, content, and other marketing tactics. This gets business owners excited about specific solutions that may not be a good fit. Especially since the author of the article doesn’t know your business and can’t accurately predict that it will work for you.</p>
+              <p>Imagine that a company produces <a href="https://diamedicalusa.com/product-category/diagnostics/vital-signs-monitors/">vital signs monitors</a> for healthcare organizations and they read an article on local SEO. They’re currently experiencing revenue problems and considering local SEO as the solution. While there may be enough demand locally, will there be enough local search volume to justify the effort and expense? It’s hard to know at first glance.</p>
+              <p>The problem is that <a href="https://www.worldometers.info/blogs/">we’re inundated with articles</a> telling us that our business needs SEO, content, and other marketing tactics. This gets business owners excited about specific solutions that may not be a good fit. Especially since the author of the article doesn’t know your business and can’t accurately predict that it will work for you.</p>
               <p>If not SEO or more content, what is the solution? If you were to ask me, I’d answer “I don’t know,” or at least “I don’t know yet.” Before trying to plug in a solution to your business challenge, it’s crucial to understand what your business really needs. After all, if a CEO thinks they need SEO, do they really need it? Or is the real need more leads? </p>
               <p>Asking a few basic questions can have a huge impact. So, what do you need to answer before setting your business strategy?
                 <ul>
@@ -41,7 +41,7 @@
               <h5>Your needs are not always what they seem</h5>
               <p>Imagine that a software development firm and a manufacturing organization both believe that an updated website will solve their marketing problems. While they’ve both arrived at a similar conclusion, do you think their business challenges are the same? Probably not. </p>
               <p>It’s natural for us to want a definite solution with a clear path forward. The problem with throwing solutions like these at your business challenges is they typically won’t give you the results you want. The point isn’t that strategies, like creating a new website or focusing on SEO are wrong — it’s that before your company pushes forward with an expensive strategy, you need to be sure that the benefits directly tie into your business objectives.</p>
-              <p>When creating their marketing strategy, businesses can overlook what their customers are looking for — and it can cost them. According to ITSMA (Information Technology Services Marketing Association), buyers are 48% more likely to consider solution providers that personalize their marketing to address their specific business issues. Putting buyers first goes hand-in-hand with an iterative strategy. </p>
+              <p>When creating their marketing strategy, businesses can overlook what their customers are looking for — and it can cost them. According to <a href="https://www.itsma.com/pdfs/research/ITSMAPersonasandB2I_AbbSum.pdf">ITSMA (Information Technology Services Marketing Association)</a>, buyers are 48% more likely to consider solution providers that personalize their marketing to address their specific business issues. Putting buyers first goes hand-in-hand with an iterative strategy. </p>
               <p>It’s easy to assume that if only your website were more responsive, more modern, people would buy more from you. But, is that really what’s holding customers back? An iterative strategy can help you find out. By running small tests, you can determine what and where customers will engage with you.</p>
               <p>There is a problem with the iterative approach though. People often equate it with being never-ending, because sometimes that is what happens. Is that really the case? How can you ensure your strategy is iterative instead of never-ending?</p>
               
@@ -52,7 +52,7 @@
           
 
               <h4>Why building a marketing machine should be your organization’s goal</h4>
-              <p>According to the U.S. Bureau of Labor Statistics, 50% of business go under by their 5th year and 80% by their 10th. Businesses like Jawbone and Juicero are clear examples that even businesses with millions in funding fail. Does this mean that smaller businesses don’t stand a chance?</p>
+              <p>According to the <a href="https://www.bls.gov/bdm/us_age_naics_00_table7.txt">U.S. Bureau of Labor Statistics</a>, 50% of business go under by their 5th year and 80% by their 10th. Businesses like <a href="https://www.cbinsights.com/research/report/hardware-startups-failure-success/">Jawbone</a> and <a href="https://www.bloomberg.com/news/features/2017-09-08/inside-juicero-s-demise-from-prized-startup-to-fire-sale">Juicero</a> are clear examples that even businesses with millions in funding fail. Does this mean that smaller businesses don’t stand a chance?</p>
               <p>Two of the main causes of failure are trouble finding clients and client dependence. Companies who build a marketing machine proactively combat this common business problem. What is a marketing machine?</p>
               <p>The marketing machine is marketing’s version of automation. It involves setting up easily repeatable processes for all of your marketing efforts including outreach, lead nurturing, closing, and post-sale communication. The goal when creating your marketing machine is to determine how people will engage with you and then iterate on that success to improve the machine’s efficiency.</p>
               <p>As an end result, you get a highly predictable marketing machine you can put money into and know what kind of ROI you’ll get from it. The benefits are clear, but still, business owners should watch for the common challenges that arise when building a marketing machine. </p>
@@ -60,7 +60,7 @@
               <h5>What are some of the issues organizations run into when building a marketing machine?</h5>
               <blockquote>
                 <span class="quote">Planning is bringing the future into the present so that you can do something about it now.</span>
-                <span class="attr">Alan Lakein, author</span>
+                <span class="attr"><a href="https://en.wikipedia.org/wiki/Alan_Lakein">Alan Lakein</a>, author</span>
               </blockquote>
 
               <p>As Lakein points out, proper planning helps you avoid challenges by resolving them before they ever come up. The same principle applies when building your marketing machine. With that in mind, let’s look at three of the most common challenges. </p>
@@ -77,14 +77,14 @@
               <p>The timeline for building your marketing machine will largely depend on the type of company you run and your industry. That said, there is a basic timeline that most companies can expect to follow.</p>
               <p><strong>The first thirty days.</strong> This is the research and discovery phase. You’ll be digging deep to identify the true challenges your business is facing. Based on those challenges, you’ll set up the initial processes and creative materials that will get the marketing machine up and running.</p>
               <p><strong>Month two.</strong> During month two, you start implementation. This is when you put into practice the initial strategy and start working out the kinks. The iterative aspect of the marketing machine is especially important during this phase. Why? Because of the newness of the strategy, you’ll see the most radical shifts and refinements.</p>
-              <p><strong>Months three and forward.</strong> From month three onward, you’ll continue refining but you’ll also start accelerating and automating the processes. By leveraging techniques like A/B testing and customer interviews, you’ll find out what’s working consistently.</p>
+              <p><strong>Months three and forward.</strong> From month three onward, you’ll continue refining but you’ll also start accelerating and automating the processes. By leveraging techniques like <a href="https://www.optimizely.com/optimization-glossary/ab-testing/">A/B testing</a> and customer interviews, you’ll find out what’s working consistently.</p>
               <p>What does this look like in practice? We’ll look at the experience of one of our clients, White Crane Design Build, a remodeler with a focus on older homes.</p>
               <p>When White Crane reached out for help, they inquired about SEO, hoping that improved SEO would generate more leads. While we didn’t think optimizing their website content would hurt, we had a hunch it wasn’t going to magically generate the desired leads. Instead, we developed a marketing test, with a focus on developing new content related to their current remodels. We then pushed that content to a highly targeted audience of potential customers to build awareness and bring in new, relevant leads. To us, this was the low hanging fruit.</p>
               <p>As this strategy started playing out, and people engaged with this new content, we gained insights that helped us further refine the strategy. It became evident that we could shift marketing dollars away from the spend bringing in low-quality leads and causing a drain on their sales and design teams.</p>
               <p>With marketing dollars shifting to more effective efforts and leads increasing, it was time to step on the gas. That initial marketing test gave us a better understanding of White Crane’s market, and we expanded this content strategy to a broader audience.</p>
               <p>The marketing machine was now up and running. Relevant leads were coming in consistently, and we had a system built for generating new content. At this point, we started consulting with White Crane’s leadership and sales team to help them better communicate through their CRM. We also developed new tools and strategies to improve conversion rates through a lead-nurturing program.</p>
               <p>By refining White Crane’s messaging, generating relevant content, and marketing to a targeted audience, we significantly improved marketing results. White Crane’s web traffic increased by 350+% and leads increased by 200+%.</p>
-              <p>It won’t be long before you start seeing results from your marketing machine. The key is to be patient as it gains traction and to continue improving along the way. But, what if your business has a unique mission? Should your business still build a marketing machine?</p>
+              <p>It won’t be long before you start seeing results from <a href="/marketing">your marketing machine</a>. The key is to be patient as it gains traction and to continue improving along the way. But, what if your business has a unique mission? Should your business still build a marketing machine?</p>
 
               <h5>Whatever mission you choose to accept, you still need revenue</h5>
               <p>Whether you want to build your business to get rich, to pass on to your family, or for an altruistic cause, one thing remains true: your business needs to make money to be successful.</p>
@@ -95,8 +95,11 @@
 
               <h4>Starting your path toward marketing that runs like clockwork</h4>
               <p>Grammarly realized that the key to success without external funding was to identify customer challenges, needs, and mindsets, and meet them there. You shouldn’t blindly copy Grammarly’s tactics, but you should copy their strategy. Our company Curious can help.</p>
-              <p>Companies can start building their marketing machine by identifying the low-hanging fruit and start acting on it with highly targeted marketing. They don’t need to break the bank to start seeing results they can build on.</p>
-          </div>
+              <p>Companies can start building their marketing machine by identifying the low-hanging fruit and start acting on it with <a href="https://www.thebalancesmb.com/target-marketing-2948355">highly targeted marketing</a>. They don’t need to break the bank to start seeing results they can build on.</p>
+
+              <div class="ctaText">
+                <span>Do you want marketing that runs like clockwork? Let’s talk about your immediate marketing opportunities and how they can build up to a larger marketing strategy. </span> <nuxt-link class="button" to="#contact">Get in touch</nuxt-link>
+              </div>
       
       </div>
     </section>
