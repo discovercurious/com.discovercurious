944e6931361dc26d57ab6d15f464247482a0c06e
Bryce Howitson <bryce@discovercurious.com>
2019-09-13 16:59:31 -0500

    adding prismic connection

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,34 +3,6 @@ import pkg from './package'
 export default {
     mode: 'universal',
     router: {
-
-        scrollBehavior: async(to, from, savedPosition) => {
-            if (savedPosition) {
-                return savedPosition
-            }
-
-            const findEl = async(hash, x) => {
-                return document.querySelector(hash) ||
-                    new Promise((resolve, reject) => {
-                        if (x > 50) {
-                            return resolve()
-                        }
-                        setTimeout(() => { resolve(findEl(hash, ++x || 1)) }, 100)
-                    })
-            }
-
-            if (to.hash) {
-                let el = await findEl(to.hash)
-                if ('scrollBehavior' in document.documentElement.style) {
-                    return window.scrollTo({ top: el.offsetTop, behavior: 'smooth' })
-                } else {
-                    return window.scrollTo(0, el.offsetTop)
-                }
-            }
-
-            return { x: 0, y: 0 }
-        },
-
         // base: process.env.NODE_ENV !== 'development' ? '/curiousnuxt/' : '/'
     },
 
@@ -99,7 +71,12 @@ export default {
     modules: [
         // Doc: https://axios.nuxtjs.org/usage
         '@nuxtjs/axios',
-        '@nuxtjs/google-analytics',
+        '@nuxtjs/google-analytics', ['prismic-nuxt', {
+            endpoint: 'https://discovercurious.cdn.prismic.io/api/v2',
+            linkResolver: function(doc, ctx) {
+                return '/insights/'
+            }
+        }]
     ],
     /*
      ** Axios module configuration
