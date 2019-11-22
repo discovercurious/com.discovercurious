6c8cf3596eb69b59e4d3972251fc9ef7a29d275b
Bryce Howitson <bryce@discovercurious.com>
2019-11-22 12:28:17 -0600

    update alomere case study

---
+++ pages/case-studies/branding-alomere-old.vue
@@ -0,0 +1,151 @@
+<template>
+  <div id="app">
+    <section class="studySection s-ImageLarge bg-white">
+      <div class="pageWidth">
+          <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/2.jpg`"></div>
+      </div>
+    </section>
+    <section class="studySection s-Intro bg-ltGray">
+      <div class="pageWidth">
+          <div class="columns s-textRow">
+            <div class="col">
+              <h3>Alomere Health Rebrand Strategy</h3>
+              <h5>Why was Curious hired?</h5>
+              <p>A county-owned health system needed help navigating a name change to better position themselves for growth.</p>
+            </div>
+            <div class="col">
+              <h5>What Curious provided</h5>
+              <p>
+                <ul>
+                  <li>Rebrand Viability Assessment</li>
+                  <li>Naming</li>
+                  <li>Identity Design</li>
+                  <li>Brand standards</li>
+                  <li>Internal Engagement Strategy</li>
+                  <li>Rebrand Launch Strategy</li>
+                  <li>Signage</li>
+                  <li>Advertising</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+      </div>
+    </section>
+    <section class="studySection s-2ColText bg-ltGray">
+      <div class="pageWidth">
+          <div class="col2 s-textRow">
+            <div class="col">
+              <h5>The Setting</h5>
+              <p>Douglas County Hospital (DCH) was the pride of the community because of its economic influence as a large employer and it’s national recognition for excellence.</p>
+              <p>To stay locally independent, DCH and three local clinics merged in 2012 but kept their individual brands. Executive director, Carl Vaagenes knew their health system needed to grow if it wanted to stay strong. The county name no longer reflected the community they served and limited future growth.</p>
+            </div>
+            <div class="col">
+              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/1.jpg`"></div>
+            </div>
+          </div>
+      </div>
+    </section>
+    <section class="studySection s-2ColText bg-ltGray">
+      <div class="pageWidth">
+          <div class="col2 s-textRow">
+            <div class="col">
+              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/ADs_1.jpg`"></div>
+            </div>
+            <div class="col">
+              <h5>The Strategy</h5>
+              <p>Curious helped Alomere Health’s leadership engage the stakeholders and the public to see the value in a change that would undoubtedly be emotional for many. Through a process of interviews, research, steering committee meetings, naming, branding, and design, Curious helped launch an exciting new chapter for a beloved community hero.</p>
+            </div>
+          </div>
+      </div>
+    </section>
+    <section class="studySection s-ImageLarge bg-white s-noBorder">
+      <div class="pageWidth">
+          <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/3.jpg`"></div>
+      </div>
+    </section>
+    <section class="studySection s-ImageGroup bg-ltGray">
+      <div class="pageWidth">
+          <div class="col2">
+            <div class="col">
+              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/BirthCert_1.jpg`"></div>
+            </div>
+            <div class="col">
+              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/Signage_1.jpg`"></div>
+            </div>
+          </div>
+          <div class="col2">
+            <div class="col">
+              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/4.jpg`"></div>
+            </div>
+            <div class="col">
+              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/5.jpg`"></div>
+            </div>
+          </div>
+      </div>
+    </section>
+        <section class="studySection s-ImageGroup bg-ltGray">
+      <div class="pageWidth">
+          <h4>The Results</h4>
+          <div class="col1">
+            <h5>The rebrand launched with great success due to a strategy that first united employees, and then inspired the community with the leadership’s vision months before the unveiling of the new name.</h5>
+          </div>
+          
+          <div class="col3">
+            <div class="col">
+              <p>We assisted Alomere Health with a rebrand rollout across all touchpoints from clinic and hospital external/internal wayfinding signage to updating all collateral and advertising materials.</p>
+              <p>Our partnership continues. By adding targeted digital marketing to a traditional media mix (print, outdoor, radio), Curious helps Alomere create health-related conversations relevant to focused audiences.</p>
+            </div>
+            <div class="col">
+              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/Signage_2.jpg`"></div>
+            </div>
+            <div class="col">
+              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/7.jpg`"></div>
+            </div>
+          </div>
+      </div>
+    </section>
+    <section class="studySection s-ImageGroup bg-darkGray">
+      <div class="pageWidth">
+          <h5>More Branding</h5>
+          <BrandingStudies />
+      </div>
+    </section>
+    <section id="cta">
+      <div class="pageWidth">
+        <div class="ctaText">
+          <span>Need help building consensus within a large organization?</span> <nuxt-link class="button" to="#contact">Get in touch</nuxt-link>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import BrandingStudies from '@/components/BrandingStudies.vue';
+
+export default {
+  components: {
+    BrandingStudies,
+  },
+  head () {
+    return {
+      // title: 'Curious: Making Products Useful, Usable & Beautiful',
+      meta: [
+        // { hid: 'description', name: 'description', content: 'Curious consultants help you identify your next best marketing steps, and develop marketing strategies to find and connect with your customers.' },
+
+        // { hid: 'keywords', name: 'keywords', content: 'product design, branding, marketing, coaching, consulting, design sprint, review, strategy, digital, identity, story, minneapolis, minnesota, twin cities, hightlight center, ux, interface' },
+         
+        // { hid: 'og:title', name: 'og:title', content: 'Curious - Solving Brand, Marketing & Product problems for small businesses' },
+        // { hid: 'og:description', name: 'og:description', content: 'Curious is a group of experienced consultants helping business leaders overcome brand, marketing, and product challenges. We help companies discover their best opportunities for growth.' },
+
+        // { hid: 'og:image', name: 'og:image', content: 'https://discovercurious.com/images/shareLogo.png' },
+        // { hid: 'og:image:type', name: 'og:image:type', content: 'image/png' },
+        // { hid: 'og:image:width', name: 'og:image:width', content: '1080' },
+        // { hid: 'og:image:height', name: 'og:image:height', content: '800' },
+      ]
+    }
+  },
+}
+</script>
+
+<style></style>
--- pages/case-studies/branding-alomere.vue
+++ pages/case-studies/branding-alomere.vue
@@ -7,39 +7,22 @@
     </section>
     <section class="studySection s-Intro bg-ltGray">
       <div class="pageWidth">
-          <div class="columns s-textRow">
+          <div class="columns half s-textRow">
+            <h3>Alomere Health Rebrand</h3>
             <div class="col">
-              <h3>Alomere Health Rebrand Strategy</h3>
-              <h5>Why was Curious hired?</h5>
-              <p>A county-owned health system needed help navigating a name change to better position themselves for growth.</p>
-            </div>
-            <div class="col">
-              <h5>What Curious provided</h5>
+              <h5>A well-loved institution ponders its name</h5>
+              <p>For sixty-plus years, the hospital in Alexandria, Minnesota, was known as Douglas County Hospital (DCH). Despite being beloved—we were told by several people that they would never call it anything else—the name had challenges:</p>
               <p>
                 <ul>
-                  <li>Rebrand Viability Assessment</li>
-                  <li>Naming</li>
-                  <li>Identity Design</li>
-                  <li>Brand standards</li>
-                  <li>Internal Engagement Strategy</li>
-                  <li>Rebrand Launch Strategy</li>
-                  <li>Signage</li>
-                  <li>Advertising</li>
+                  <li>DCH served more than the Douglas area, with a reach that extended nine counties and beyond.</li>
+                  <li>Its leading-care approach and excellent outcomes didn’t fit a &ldquo;county hospital&rdquo; image.</li>
+                  <li>And it was no longer just a hospital, with 3 clinics and other specialty services.</li>
                 </ul>
               </p>
             </div>
-          </div>
-      </div>
-    </section>
-    <section class="studySection s-2ColText bg-ltGray">
-      <div class="pageWidth">
-          <div class="col2 s-textRow">
             <div class="col">
-              <h5>The Setting</h5>
-              <p>Douglas County Hospital (DCH) was the pride of the community because of its economic influence as a large employer and it’s national recognition for excellence.</p>
-              <p>To stay locally independent, DCH and three local clinics merged in 2012 but kept their individual brands. Executive director, Carl Vaagenes knew their health system needed to grow if it wanted to stay strong. The county name no longer reflected the community they served and limited future growth.</p>
-            </div>
-            <div class="col">
+              <p>Because of these issues, the name hindered the ability to evolve easily with the rapid changes in healthcare, and it was challenging to communicate the amazing care the organization provided across multiple facilities and service lines. Carl Vaagenes, CEO, and the board knew that stellar community-owned healthcare could benefit from a presence that its current name constrained.</p>
+              <p>The hard question was whether to change a name steeped with local history and generations-long relationships.</p>
               <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/1.jpg`"></div>
             </div>
           </div>
@@ -49,30 +32,66 @@
       <div class="pageWidth">
           <div class="col2 s-textRow">
             <div class="col">
-              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/ADs_1.jpg`"></div>
+              <h5>Checking the temperature for a change.</h5>
+              <p>At the start of Curious’ engagement, we spent an hour each with 33 stakeholders of DCH. This wasn’t our original plan; the proposal included 12 interviews. But the further we got, the more we realized how important it was to hear prominent voices around an organization for which the community felt deep ownership. Three of the 33 interviewees said they would never support a name change, and another nine were on the fence, saying their support depended on what the new name was.</p>
             </div>
             <div class="col">
-              <h5>The Strategy</h5>
-              <p>Curious helped Alomere Health’s leadership engage the stakeholders and the public to see the value in a change that would undoubtedly be emotional for many. Through a process of interviews, research, steering committee meetings, naming, branding, and design, Curious helped launch an exciting new chapter for a beloved community hero.</p>
+              <p>So over a third of the county commissioners, local leaders and physicians were lukewarm-to-cold on the idea of name change. With a task to recommend &ldquo;go or no go&rdquo; on a new name, never had we embarked on a potential naming decision with such a headwind. At the same time, our interviews showed us what a remarkably effective, forward-looking, team-oriented and pragmatic culture DCH had built.</p>
+              <p>We worked to confirm our interview discoveries with quantitative research. A response rate of 56% from the staff and 43% from the community was a clear signal that DCH had incredible engagement. Whether the name was left intact or changed, the decision was sure to be a hot topic.</p>
             </div>
           </div>
       </div>
     </section>
+
     <section class="studySection s-ImageLarge bg-white s-noBorder">
       <div class="pageWidth">
           <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/3.jpg`"></div>
       </div>
     </section>
-    <section class="studySection s-ImageGroup bg-ltGray">
+
+    <section class="studySection s-2ColText bg-ltGray">
       <div class="pageWidth">
-          <div class="col2">
+          <div class="col2 s-textRow">
             <div class="col">
-              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/BirthCert_1.jpg`"></div>
+              <h5>Trusting the process.</h5>
+              <p>Our final recommendation was to go forward with the rename. The research spoke to wanting a name that truly felt like who DCH was, a single name that would work across clinics and create a more cohesive culture, a name that would allow the organization to serve the area and be nimble to meet changing needs.</p>
+              <p>With DCH being of-the-community and for-the-community, the name couldn’t just be picked by a select few, so Curious partnered with 3.2.1 for this project, using a data- driven tool called SparkTM. The Spark process allows diverse groups to speak their mind as it sorts and ranks the choices over 21 metrics.</p>
+              <p>How diverse a group? For DCH, our first meeting to present 20 names (from a list of over 250) included 17 people. The process also reduced the power of power; with physicians, the CEO and board chair in the room, it was important to give everyone participating an equal footing. The Spark process does that.</p>
+              <p>This is also where the process discovered a blindspot.</p>
             </div>
             <div class="col">
-              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/Signage_1.jpg`"></div>
+              <h5>Seeing beyond the obvious.</h5>
+              <p>As options were sorted, the leading names had a vague similarity. Alexandria is renowned for being nestled within a constellation of lakes. The names that rose to the top had &ldquo;lake&rdquo; in them in some form—making them indistinguishable from each other... and every other business in lakeland.</p>
+              <p>This remarkable hospital system would not be well-served by an unremarkable name.</p>
+              <p>With this aha, we went back to generate another 250 names.</p>
+              <h5>Letting the cream rise.</h5>
+              <p>The final three names jumped through Spark process hoops to make sure they fit with what we discovered from our research and the organization’s goals for the name. The three finalists then went on a tour of 10 community meetings, where we shared research findings as well as the hopes and dreams for the organization.</p>
+              <p>The chosen name—Alomere Health—combines the idea of &ldquo;nurture&rdquo; and &ldquo;strengthen&rdquo;—alo in Latin—with the English word mere or lake. The &ldquo;healing waters&rdquo; of Alomere Health spoke to the lakes of the area, and also the core values of the organization.</p>
             </div>
           </div>
+      </div>
+    </section>
+
+    <section class="studySection s-2ColText bg-ltGray">
+      <div class="pageWidth">
+          <div class="col2 s-textRow">
+            <div class="col">
+              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/ADs_1.jpg`"></div>
+              <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/Signage_1.jpg`"></div>
+            </div>
+            <div class="col">
+              <h5>The ending is the beginning.</h5>
+              <p>The selection of Alomere Health set a critical piece in motion—an effective rollout that understands the difficulty of change. Months before the launch, Curious helped clinics and hospital leadership draft letters to the community, outlining a bold vision for healthcare locally, with a new name as one step on the journey. Come launch day, as signage around the campuses changed, we continued the conversation on social media.</p>
+              <p>But a key element was inclusivity.</p>
+              <p>Carl sent out an open call, inviting all interested staff to be ambassadors for the new name. This team created events and activities that helped the hospital share its story and vision, and inoculate itself against a natural human urge to resist even the best changes. The same team still meets, plans and celebrates, a testament to Alomere Health’s remarkable team-oriented culture.</p>
+              <p>For Curious’ part, job one was to switch-out the countless places the logo appeared, but our success hinged on strengthening the voice and look of Alomere Health. We used discoveries from research to inform language and design so that all outward expressions of the brand could better reflect what was true within.</p>
+            </div>
+          </div>
+      </div>
+    </section>
+
+    <section class="studySection s-ImageGroup bg-ltGray">
+      <div class="pageWidth">
           <div class="col2">
             <div class="col">
               <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/4.jpg`"></div>
@@ -85,15 +104,16 @@
     </section>
         <section class="studySection s-ImageGroup bg-ltGray">
       <div class="pageWidth">
-          <h4>The Results</h4>
+          <h4>What we learned by being Curious.</h4>
           <div class="col1">
             <h5>The rebrand launched with great success due to a strategy that first united employees, and then inspired the community with the leadership’s vision months before the unveiling of the new name.</h5>
           </div>
           
           <div class="col3">
             <div class="col">
-              <p>We assisted Alomere Health with a rebrand rollout across all touchpoints from clinic and hospital external/internal wayfinding signage to updating all collateral and advertising materials.</p>
-              <p>Our partnership continues. By adding targeted digital marketing to a traditional media mix (print, outdoor, radio), Curious helps Alomere create health-related conversations relevant to focused audiences.</p>
+              <p>Naming isn’t for the easily daunted. It’s a personal, often emotion-driven, worry- fraught choice in which &ldquo;I just don’t like it,&rdquo; is an easy way to dismiss the best group conversations.</p>
+              <p>What was reinforced for us was the need to ground the process well—in the identity, culture, community, and goals and aspirations of the organization. These touchstones help raise the process above our personal tastes to help discover, collectively, what truly resonates.</p>
+              <p>For Alomere Health, the name was quickly embraced, even by those resistant to change. After a year, the organization’s name was natural and right—in the community and beyond—allowing Alomere Health to add specialties, reach new clients and positively affect the health of the entire area for decades.</p>
             </div>
             <div class="col">
               <div class="responsiveImage" v-lazy:background-image="`/casestudies/alomereBrand/Signage_2.jpg`"></div>
