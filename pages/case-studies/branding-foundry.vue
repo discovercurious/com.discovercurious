87a332da024303d3c42f139089687f14862ac191
Bryce Howitson <bryce@discovercurious.com>
2020-06-01 09:19:30 -0500

    working to fix reload error

--- pages/case-studies/branding-foundry.vue
+++ pages/case-studies/branding-foundry.vue
@@ -100,24 +100,24 @@ export default {
   components: {
     BrandingStudies,
   },
-  head () {
-    return {
-      // title: 'Curious: Making Products Useful, Usable & Beautiful',
-      meta: [
-        // { hid: 'description', name: 'description', content: 'Curious consultants help you identify your next best marketing steps, and develop marketing strategies to find and connect with your customers.' },
+  // head () {
+  //   return {
+  //     title: 'Curious: Making Products Useful, Usable & Beautiful',
+  //     meta: [
+  //       { hid: 'description', name: 'description', content: 'Curious consultants help you identify your next best marketing steps, and develop marketing strategies to find and connect with your customers.' },
 
-        // { hid: 'keywords', name: 'keywords', content: 'product design, branding, marketing, coaching, consulting, design sprint, review, strategy, digital, identity, story, minneapolis, minnesota, twin cities, hightlight center, ux, interface' },
+  //       { hid: 'keywords', name: 'keywords', content: 'product design, branding, marketing, coaching, consulting, design sprint, review, strategy, digital, identity, story, minneapolis, minnesota, twin cities, hightlight center, ux, interface' },
          
-        // { hid: 'og:title', name: 'og:title', content: 'Curious - Solving Brand, Marketing & Product problems for small businesses' },
-        // { hid: 'og:description', name: 'og:description', content: 'Curious is a group of experienced consultants helping business leaders overcome brand, marketing, and product challenges. We help companies discover their best opportunities for growth.' },
+  //       { hid: 'og:title', name: 'og:title', content: 'Curious - Solving Brand, Marketing & Product problems for small businesses' },
+  //       { hid: 'og:description', name: 'og:description', content: 'Curious is a group of experienced consultants helping business leaders overcome brand, marketing, and product challenges. We help companies discover their best opportunities for growth.' },
 
-        // { hid: 'og:image', name: 'og:image', content: 'https://discovercurious.com/images/shareLogo.png' },
-        // { hid: 'og:image:type', name: 'og:image:type', content: 'image/png' },
-        // { hid: 'og:image:width', name: 'og:image:width', content: '1080' },
-        // { hid: 'og:image:height', name: 'og:image:height', content: '800' },
-      ]
-    }
-  },
+  //       { hid: 'og:image', name: 'og:image', content: 'https://discovercurious.com/images/shareLogo.png' },
+  //       { hid: 'og:image:type', name: 'og:image:type', content: 'image/png' },
+  //       { hid: 'og:image:width', name: 'og:image:width', content: '1080' },
+  //       { hid: 'og:image:height', name: 'og:image:height', content: '800' },
+  //     ]
+  //   }
+  // },
 }
 </script>
 
